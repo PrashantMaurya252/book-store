8a9e17732ab7b434286da3dc5455ddde0d478697
PrashantMaurya252 <mauryaprashant252@gmail.com>
2023-11-24 08:41:43 +0530

    creating layout

---
+++ script.js
@@ -0,0 +1,18 @@
+// https://www.googleapis.com/books/v1/volumes?q=quilting
+
+// Step1 -Read User Input
+
+
+
+// Step-2 Create the request
+
+
+
+
+// Step-3 Send the request and receive response
+
+
+// Convert response to js object
+
+
+// Desiplay on the front end
